6c5412376a3d3e67984639a14ab95b1d96f1ea72
Profe Matias Prestes <profematiasprestes@gmail.com>
2025-04-12 23:31:53 -0300

    inicio

--- src/data/userData.js
+++ src/data/userData.js
@@ -1,58 +1,33 @@
 const userData = [
   {
-    userName: "Sofia Osorio",
-    userDesc: "Web Developer",
-    userPic: "https://github.com/SofiDevO.png",
+    userName: "Galia Prestes Garcia",
+    userDesc: "Mi Primer Año",
+    userPic: "/img/Galia.png",
     userPicDark: "/img/pedro_raccon.gif",
   },
 ];
 
 
 
-
 const socialLinks = [
-  {
-    socialName: "LinkedIn",
-    socialUl: "#",
-    socialIcon: "mingcute:linkedin-fill",
-  },
-  {
-    socialName: "GitHub",
-    socialUl: "https://github.com/SofiDevO/allmylinks",
-    socialIcon: "fa6-brands:github-alt",
-  },
-  {
-    socialName: "Instagram",
-    socialUl: "#",
-    socialIcon: "ri:instagram-fill",
-  },
-  {
-    socialName: "X",
-    socialUl: "#",
-    socialIcon: "prime:twitter",
-  },
-  {
-    socialName: "Whatsapp",
-    socialUl: "#",
-    socialIcon: "mingcute:whatsapp-fill",
-  },
-];
+    
+]
 
 const userLinks = [
     {
-        title: "Portfolio",
-        link:"https://itssofi.dev/",
+        title: "Confirma Asistencia",
+        link:"#",
         icon:"ant-design:code-filled"
     },
     {
-        title: "Sucríbete a mi canal",
-        link:"https://www.youtube.com/channel/UC36_js-krsAHAEAWpEDhHtw",
+        title: "Ubicación",
+        link:"#",
         icon:"mingcute:youtube-fill"
 
     },
     {
-        title: "Únete a Discord",
-        link:"https://discord.com/invite/wjERezH2",
+        title: "Regalos",
+        link:"#",
         icon:"ic:outline-discord"
     },
 ]
